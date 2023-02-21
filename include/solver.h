f57050cad4ea65d356e9b4ea290bd2181cee0cfa
Justine Loizel <justine.loizel@gmail.com>
2023-02-21 12:56:28 +0100

    ADD: .h files added and folders src created

---
+++ include/solver.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** B-CPE-200-REN-2-1-dante-tom.lefoix
+** File description:
+** solver.h
+*/
+
+#ifndef SOLVER_H_
+    #define SOLVER_H_
+    #include "my.h"
+    #include "myprintf.h"
+
+
+#endif
