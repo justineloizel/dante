12523c4b908ba86621a650ecb6e68e12360fc5dc
Justine Loizel <justine.loizel@gmail.com>
2023-03-01 13:34:44 +0100

    FIX: coding style errors in generator.h

--- include/generator.h
+++ include/generator.h
@@ -4,12 +4,23 @@
 ** File description:
 ** generator.h
 */
-#ifndef GENERATOR_H_
-	#define GENERATOR_H_
-int generator(int nb_line, int nb_column, int is_perfect);
-void create_perfect_maze(char **maze, int nb_line, int nb_column);
-void create_imperfect_maze(char **maze);
 
+#ifndef GENERATOR_H_
+    #define GENERATOR_H_
+    #define DIR_EAST maze[info->i][info->j + 1] = WAY,\
+    maze[info->i][info->j + 2] = WAY, \
+    info->j += 2
+    #define DIR_WEST maze[info->i][info->j - 1] = WAY,\
+    maze[info->i][info->j - 2] = WAY,\
+    info->j -= 2
+    #define DIR_NORTH  maze[info->i - 1][info->j] = WAY,\
+    maze[info->i - 2][info->j] = WAY,\
+    info->i -= 2
+    #define DIR_SOUTH maze[info->i + 1][info->j] = WAY,\
+    maze[info->i + 2][info->j] = WAY,\
+    info->i += 2
+    #define EAST_POS info->j < info->nb_column - 2
+    #define SOUTH_POS info->i < info->nb_line - 2
 typedef enum element {
     WALLS = 'X',
     WAY = '*'
@@ -25,8 +36,6 @@ enum direction {
     SOUTH,
     WEST,
 };
-
-//DFS
 typedef struct node {
     int i;
     int j;
@@ -46,22 +55,11 @@ typedef struct info_pos {
     int nb_column;
     int table_possibility[4];
 }info_pos_t;
-#define DIR_EAST maze[info->i][info->j + 1] = WAY,\
-maze[info->i][info->j + 2] = WAY, \
-info->j += 2;
-#define DIR_WEST maze[info->i][info->j - 1] = WAY,\
-maze[info->i][info->j - 2] = WAY,\
-info->j -= 2;
-#define DIR_NORTH  maze[info->i - 1][info->j] = WAY,\
-maze[info->i - 2][info->j] = WAY,\
-info->i -= 2;
-#define DIR_SOUTH maze[info->i + 1][info->j] = WAY,\
-maze[info->i + 2][info->j] = WAY,\
-info->i += 2;
-#define EAST_POS info->j < info->nb_column - 2
-#define SOUTH_POS info->i < info->nb_line - 2
+
+int generator(int nb_line, int nb_column, int is_perfect);
+void create_perfect_maze(char **maze, int nb_line, int nb_column);
+void create_imperfect_maze(char **maze);
 void delete_node(linked_list_t *list, node_t *target);
 void add_node(linked_list_t *list, int i, int j);
 void create_maze_with_dsf(char** maze, int nb_column, int nb_line);
-#endif /*GENERATOR_H_*/
-
+#endif
